0f098e5b85eaddda0a4f29f027db3b8d6c0c6eb7
Frank van Wijk <info@frankvanwijk.nl>
2025-05-04 23:01:14 +0200

    Move garden management from home to My Garden page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,8 @@
-import { usePlants } from "../hooks/usePlants";
-
 export const HomePage = () => {
-  const { data: plants } = usePlants();
-
   return (
     <div className="container p-4">
-      <h1 className="text-5xl font-bold mb-4">Mijn planten</h1>
-      <ul className="list-disc list-inside">
-        {plants?.map((plant) => (
-          <li key={plant.name}>{plant.name}</li>
-        ))}
-      </ul>
+      <h1 className="text-5xl font-bold mb-4">Tuin app</h1>
+      <p>Beheer je tuin als een baas.</p>
     </div>
   );
 };
--- src/pages/MyGardenPage.tsx
+++ src/pages/MyGardenPage.tsx
@@ -1,8 +1,16 @@
+import { usePlants } from "../hooks/usePlants";
+
 export const MyGardenPage = () => {
+  const { data: plants } = usePlants();
+
   return (
     <div className="container p-4">
-      <h1 className="text-5xl font-bold mb-4">Mijn Tuin</h1>
-      <p>Hier vindt u informatie over uw eigen tuin en planten.</p>
+      <h1 className="text-5xl font-bold mb-4">Mijn tuin</h1>
+      <ul className="list-disc list-inside">
+        {plants?.map((plant) => (
+          <li key={plant.name}>{plant.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
